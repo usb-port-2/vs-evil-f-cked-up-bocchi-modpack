20f5245076a41a7ce8fe4e8ac9456189824c006b
Lily <egzozu.be.bas@gmail.com>
2024-11-22 18:57:42 +0300

    yeah fuck it

--- data/global.hx
+++ data/global.hx
@@ -20,7 +20,7 @@ function new(){
 function preStateSwitch()
 	for (redirectState in redirectStates.keys()) 
 		if (Std.isOfType(FlxG.game._requestedState, redirectState)) 
-			FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));}
+			FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
 
 function postStateSwitch()
 	if(Framerate.codenameBuildField.visible == true) Framerate.codenameBuildField.visible = false;
